5e883b6673e34c6a6908ace6266b86d8384ef0a9
Ostrish <mac@ostrish.local>
2016-07-06 09:45:32 +0800

    Initial Commit

---
+++ NSThread_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSThread_Demo
+//
+//  Created by Ostrish on 16/7/6.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSThread_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSThread_Demo
+//
+//  Created by Ostrish on 16/7/6.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
